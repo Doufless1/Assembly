18e6022cf4e233bb221e5acfaa2f35559b688900
Doufless1 <552608@student.saxion.nl>
2024-11-25 20:20:45 +0100

    I am trying to add it for multiple digits but its not finished I cant convert it back to ASCII so it prints to the Console

--- basic_mathematical_operations.asm
+++ basic_mathematical_operations.asm
@@ -30,9 +30,12 @@ mov rsi, number1
 mov rdx, 4
 syscall
 
-  movzx rax, byte [number1] ; Load the ASCII character into rax and zero-extend
-    sub rax, '0'
+  ;movzx rax, byte [number1] ; Load the ASCII character into rax and zero-extend
+  ;  sub rax, '0'
 
+mov rsi,number1
+mov rax, 0 
+call loop_for_adding_the_numbers ; the call instruction pushes the address of the next instruction after the call onto the stack
 mov rbx,rax
 
 mov rax, 1
@@ -47,18 +50,22 @@ mov rsi, number2
 mov rdx, 4
 syscall
 
-  movzx rax, byte [number2] ; Load the ASCII character into rax and zero-extend
-    sub rax, '0'
+mov rsi,number2
+mov rax, 0 
+call loop_for_adding_the_numbers 
 
 add rax,rbx
 
+mov rsi,rax
+mov rax,0
+call make_it_ASCII
+mov [resultBuffer],rax
+
 add rax, '0'
 
-mov [resultBuffer],rax
 
 ;movzx rax,byte [resultBuffer]
 ;add rax, '0'
-
 
 mov rax,1
 mov rdi,1
@@ -72,8 +79,40 @@ mov rsi,resultBuffer
 mov rdx,20
 syscall
 
+jmp return_0
+
+mov rax, 1
+mov rdi, 1
+mov rsi, resultBuffer
+mov rdx, 20
+syscall
+
+jmp return_0
+
+
+loop_for_adding_the_numbers:
+movzx rcx, byte [rsi]
+cmp rcx, 0x0A
+je we_found_every_digit_inside_variable
+sub rcx, '0'
+imul rax,rax,10
+add rax,rcx
+inc rsi ; this points tot he next memory like if we have lets say '1','2','3' if we dont add it points to the first one
+jmp loop_for_adding_the_numbers
+
+
+return_0:
 mov rax,60
 mov rdi,0
 syscall
 
+we_found_every_digit_inside_variable:
+ret
+
+
+make_it_ASCII:
+div rax,10
+add rax, '0'
+inc rsi
+jmp make_it_ASCII
 
